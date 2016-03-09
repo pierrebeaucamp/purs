92fb84fc51c136b71f21c34aa78a0b93ae62ec43
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-03-09 17:31:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Purse
+
+A build tool for PureScript, based on npm - Why the heck should I use bower?
+**Work in progress**
---
+++ purs.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+require('shelljs/make');
+
+target.all = function() {
+    echo("TODO: Print help");
+};
+
+target.build = function() {
+    echo("To be implemented");
+};
+
+target.fmt = function() {
+    echo("To be implemented");
+};
+
+target.test = function() {
+    echo("To be implemented");
+};
